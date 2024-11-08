294647535d6fbe1f042e177f111bcd8259932c48
Kachurina Irina <i.kachurina@nexusverse.io>
2024-11-08 18:53:19 +0300

    icon replacement

--- javascripts/discourse/initializers/initialize-icon-replacement.js
+++ javascripts/discourse/initializers/initialize-icon-replacement.js
@@ -2,4 +2,7 @@ import { apiInitializer } from "discourse/lib/api";
 
 export default apiInitializer("0.11.1", (api) => {
   api.replaceIcon("d-tracking", "bs-bell");
+  api.replaceIcon("d-watching-first", "bs-bell-one");
+  api.replaceIcon("d-muted", "bs-bell-slash");
+  api.replaceIcon("d-regular", "bs-far-bell");
 });
