2da00be2f720f970c4ff994b3344e14d3606f161
Kachurina Irina <i.kachurina@nexusverse.io>
2024-11-27 02:44:35 +0300

    category group in progress

--- javascripts/discourse/initializers/initialize-category-boxes.js
+++ javascripts/discourse/initializers/initialize-category-boxes.js
@@ -1,6 +1,6 @@
 import { apiInitializer } from "discourse/lib/api";
-import CustomCategoryBoxes from "../components/custom-category-boxes";
+import CustomCategoryBoxes from "../components/custom-category-boxes_old";
 
 export default apiInitializer("1.14.0", (api) => {
-  api.renderInOutlet("above-discovery-categories", CustomCategoryBoxes);
+  //api.renderInOutlet("above-discovery-categories", CustomCategoryBoxes);
 });
